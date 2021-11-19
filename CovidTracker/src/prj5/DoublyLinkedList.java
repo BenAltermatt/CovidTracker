78cd6ef715e8e7d43621d61293ed0d7027fdad61
BenAltermatt <92002939+BenAltermatt@users.noreply.github.com>
2021-11-18 23:29:01 -0500

    Project Files Made
    Only finished writing one test class though. And I haven't made the comparator classes.

---
+++ CovidTracker/src/prj5/DoublyLinkedList.java
@@ -0,0 +1,20 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who
+// do.
+// -- Benjamin Altermat, benaltermatt@vt.edu, 906347458
+package prj5;
+
+import java.util.Iterator;
+
+public class DoublyLinkedList<E> implements Iterable<E>{
+
+    private static class Node<E> {
+        
+    }
+    
+    private class DLLIterator<A> implements Iterator<E> {
+        
+    }
+}
