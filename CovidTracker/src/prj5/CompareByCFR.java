932f79bb6c213f164f9c99c08f3e84b8c863606e
BenAltermatt <92002939+BenAltermatt@users.noreply.github.com>
2021-11-19 22:56:32 -0500

    Finished CompareByCFR

--- CovidTracker/src/prj5/CompareByCFR.java
+++ CovidTracker/src/prj5/CompareByCFR.java
@@ -8,9 +8,35 @@ package prj5;
 
 import java.util.Comparator;
 
-public class CompareByCFR implements Comparator<RaceGroup>{
-    
+/**
+ * This class exists to make it easier to sort
+ * RaceGroups by CFR.
+ * 
+ * @author Benjamin Altermatt
+ * @version 2021.11.19
+ */
+public class CompareByCFR implements Comparator<RaceGroup> {
+
+    /**
+     * Returns a value that is lesser if the first group
+     * should be sorted earlier than the second, and greater
+     * if the second should be sorted earlier than the first.
+     * 0 if they have the same sort position.
+     * 
+     * @return
+     *         Comparison value
+     */
     public int compare(RaceGroup group1, RaceGroup group2) {
-        return -1;
+        double difCFR = group2.getCFR() - group1.getCFR();
+
+        if (difCFR < 0) {
+            return -1;
+        }
+        if (difCFR > 0) {
+            return 1;
+        }
+
+        CompareByAlpha auxComparer = new CompareByAlpha();
+        return auxComparer.compare(group1, group2);
     }
 }
